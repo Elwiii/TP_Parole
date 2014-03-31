d8f209783324d8d0f7adf6644fed3615a8f2c97e
Nikolai <Nikolai@10.10.127.224>
2014-03-31 12:01:20 +0200

    ²

--- src/tp1/TP1.java
+++ src/tp1/TP1.java
@@ -27,6 +27,10 @@ public class TP1 {
             signal.setSignal("test_seg.wav");
             signal_tab = signal.getSignal();
             double[] energies = signalEnergy(8, 30);
+            for (int i = 0; i < energies.length; i++) {
+                System.out.println(i+" : "+energies[i]);
+                
+            }
             System.out.println("energies : "+energies);
             System.out.println("Length of the signal (in sample) : " + signal.getSignalLength());
             System.out.println("Sampling frequency : " + signal.getSamplingFrequency());
@@ -39,42 +43,37 @@ public class TP1 {
 
     }
     
-//    System.out.println("Length of the signal (in sample) : " + signal.getSignalLength());
-//            System.out.println("Sampling frequency : " + signal.getSamplingFrequency());
-//            System.out.println("Length of the signal (in ms) : " + signal.getSignalLength() * 1000 / signal.getSamplingFrequency());
-//            
-    
     public static double[] signalEnergy(int m,int N){
-//        int signalLength = signal.getSignalLength();
         int signalLenghtMs = signal.getSignalLength() * 1000 / signal.getSamplingFrequency();
-        
         int nb_fenetre = signalLenghtMs/N;
         System.out.println("nb_fenetre : "+nb_fenetre);
         double[] nrjs = new double[nb_fenetre];
-        
         int taille_fenetre_tab = N * signal_tab.length / signalLenghtMs;
         int taille_decalage_tab = m * signal_tab.length / signalLenghtMs;
-        
-        
         int i_fenetre = 0;
         int j = 0 ;
         while(i_fenetre < nb_fenetre){
             System.out.println(""+i_fenetre);
+            System.out.println("j : "+j);
             int bornesup = taille_decalage_tab * i_fenetre + taille_fenetre_tab;
             while(j < bornesup){
-                double nrj = 0;
                 // on calcul l'énergie
-//                System.out.println("-------------------");
-//                System.out.println("-------------------");
-//                System.out.println("i_fenetre : "+i_fenetre);
-//                System.out.println("j : "+j);
+                nrjs[i_fenetre]=computeEnergy(j,i_fenetre);
                 j++;
             }
             i_fenetre++;
             j = taille_decalage_tab * i_fenetre;
         }
-        
         return nrjs;
     }
-    
+
+
+    private static double computeEnergy(int n, int nw){
+        double somme = 0;
+        for (int k = -nw/2; k < nw/2; k++) {
+            somme += Math.pow(signal_tab[n+k],2);
+        }
+        somme *= (1/(nw+1));
+        return somme;
+    }
 }
